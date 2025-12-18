f6758b293cad1adbf3a36f7bf6efbfd1173bd52e
Krishan Mohan <krishanmohan3501@gmail.com>
2025-12-18 10:12:08 +0530

    cap training

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+/*
+    Java is a High level programming languuage , which is easily readable , understandbale and instructable by programmer.
+    Why Java is Plateform independent Language?
+    - Becuase in java source code is converted into Byte Code which is easily executable in any platform which has JVM.
+    - JVM stands for Java Virtual Machine, it is physically does not exist, it is used to convert byte code into binary code.
+    - It consist of JIT and Interpreter
+    - JRE it stands for Java Runtime Environment, it physically exists. It is used to Provide Environment to run java Program
+    - JDK it stands for Java Development Kit , It physically exists . It Provide an environment to run as well as develop java program.
+    - System.out.print("data"); after printing the data the cursor is shifted to next position in same line
+    - System.out.println("MOhan"); after printing the data the cursor is shifted to next postion in new line
+
+
+     */
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("10");
+        System.out.print("20");
+        System.out.println("30");
+        System.out.println("40");
+        System.out.println("50");
+        System.out.print("60");
+        System.out.print("70");
+        System.out.println("80");
+        System.out.println("90");
+        System.out.print("100");
+        System.out.println("110");
+        System.out.print("120");
+        System.out.print("_");
+
+    }
+}
