cdbae783b690b2823d47d217c679c6babcfac53b
Krishan Mohan <krishanmohan3501@gmail.com>
2025-12-18 12:01:57 +0530

    Types of variable

--- src/Main.java
+++ src/Main.java
@@ -10,25 +10,142 @@
     - System.out.println("MOhan"); after printing the data the cursor is shifted to next postion in new line
 
 
+    Token
+    -the smallest part of java program is known as Tokens.
+    Types of Tokens
+    -Keywords, Identifiers, Literals, Seperators, Operators
+    Keywords
+    - A word which are having predefine meaning is known as keywords.
+    - It is also known as compiler aware Words.
+    - Keywords are always should be in lowercase.
+    - In java we have 50+ keywords
+    - examples class, public , void, print, System
+    Identifiers
+    - The name given by the programmer is know as Identifier.
+    Rules
+    - We cannot use keyword as Identifier.
+    - we cannot use space .
+    - we can't start with numbers.
+    - Special Charaters are not allowrd except underscore(_) and dollar($).
+    Types of Identifiers
+    -ClassName, VariableName, MethodName, PackageName.
+    Literals
+    - The value or data given the programmer is known as Literals.
+    Types of Literals
+    Number
+    - Interger
+    - Floating
+    Character
+    - It is used to represent a single data, it is enclosed inside single quote.
+    - Length of characte literal is one .
+    - We can use UpperCase, LowerCase character, Digits and all type of special Charaters.
+    String
+    - It is used to represent group of characters
+    - It is enclosed inside double quotes ""
+    - The length of String literal can be anything
+    Boolean
+    - Boolean uses only two value either True or False
+    Seperators
+    - It used to seperate JAVA Members
+    Types  of Seperators
+    - block {}
+    - () -> eaution, methods,declaration, conditions
+    - ; -> end of the statement
+    - , -> Varibale
+    - . Reference
+
+    DataType
+    - used to define sixe and type of data
+    Primitive DataType
+    -Inbuilt datatype
+    - Fixed Size
+    Integer
+    - byte - 1 byte -> 8 bits
+    - short -> 2 byte -> 16bits
+    - int -> 4 byte -> 32 bits
+    - long -> 8 byte -> 64 bits
+    Float
+    - Float -> 4 byte -> 32 bits
+    - Double -> 8 byte -> 64 bits
+
+    Character
+    char -> 2 byte -> 16 bits
+    Booleam  -> 1bit
+
+    Non-Primitive
+    - does not have fixed size
+    - user defined
+    - String
+    - Array
+    - ClassName
+
+    Varibale
+    Type of Variable
+    Based On DateType
+    - Primitive
+    - Non-Primitive
+    Based on Scope
+    - Local
+    - Global
+        - static
+        - Non static
+
+
+
+
+
+
+
      */
 
 
 
 public class Main {
+
+    // class area or global area
+    int a = 23;
+    static  int b = 34;
+
     public static void main(String[] args) {
-        System.out.print("10");
-        System.out.print("20");
-        System.out.println("30");
-        System.out.println("40");
-        System.out.println("50");
-        System.out.print("60");
-        System.out.print("70");
-        System.out.println("80");
-        System.out.println("90");
-        System.out.print("100");
-        System.out.println("110");
-        System.out.print("120");
+//        System.out.print("10");
+//        System.out.print("20");
+//        System.out.println("30");
+//        System.out.println("40");
+//        System.out.println("50");
+//        System.out.print("60");
+//        System.out.print("70");
+//        System.out.println("80");
+//        System.out.println("90");
+//        System.out.print("100");
+//        System.out.println("110");
+//        System.out.print("120");
+//        System.out.print("_");
+
+        System.out.print("5");
+        System.out.print("25");
+        System.out.print("75");
+        System.out.println("85");
+        System.out.print("25");
+        System.out.print("45");
+        System.out.println("35");
+        System.out.print("15");
+        System.out.println("20");
+        System.out.print("5");
+        System.out.print("66");
+        System.out.print("67");
         System.out.print("_");
+
+        // Local area or method area
+
+
+
+
+
+
+
+
+
+
 
     }
 }
